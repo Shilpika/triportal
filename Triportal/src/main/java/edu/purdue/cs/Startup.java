1b9a435a391eac5de9bad948f93b9d38f035deef
Yibo Gou <i@bowiegou.com>
2016-01-23 16:57:09 -0500

    Setting up base style, added startup page

--- Triportal/src/main/java/edu/purdue/cs/Startup.java
+++ Triportal/src/main/java/edu/purdue/cs/Startup.java
@@ -9,27 +9,37 @@
 package edu.purdue.cs;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.parse.ParseAnalytics;
 
 
-public class MainActivity extends AppCompatActivity {
+public class Startup extends AppCompatActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-
+    setContentView(R.layout.activity_startup);
     ParseAnalytics.trackAppOpenedInBackground(getIntent());
+    Toolbar actionbar = (Toolbar) findViewById(R.id.startupActionBar);
+    setSupportActionBar(actionbar);
+    getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+    //TODO: Turn this into ViewPage, The contents will be in tabs
+
   }
 
   @Override
   public boolean onCreateOptionsMenu(Menu menu) {
     // Inflate the menu; this adds items to the action bar if it is present.
-    getMenuInflater().inflate(R.menu.menu_main, menu);
+    getMenuInflater().inflate(R.menu.menu_starup, menu);
     return true;
   }
 
@@ -41,10 +51,40 @@ public class MainActivity extends AppCompatActivity {
     int id = item.getItemId();
 
     //noinspection SimplifiableIfStatement
-    if (id == R.id.action_settings) {
+    if (id == R.id.action_user_settings) {
       return true;
     }
 
     return super.onOptionsItemSelected(item);
   }
+
+
+  public static class AppSectionsPagerAdapter extends FragmentPagerAdapter {
+    public AppSectionsPagerAdapter(FragmentManager fm) {
+      super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+      switch (i) {
+        case 0:
+          //TODO
+          return null;
+
+        default:
+          return null;
+      }
+    }
+
+    @Override
+    public int getCount() {
+      return 2; //number of tabs will be added
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+      return "Section " + (position + 1);
+    }
+  }
 }
+
