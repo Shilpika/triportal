485b28da6993e867c004d9665053be3e66f1afbe
Rix <i@rix.li>
2016-02-01 20:29:41 -0500

    Implement basic ParseLogin support

--- Triportal/src/main/java/edu/purdue/cs/SplashScreen.java
+++ Triportal/src/main/java/edu/purdue/cs/SplashScreen.java
@@ -1,20 +1,11 @@
 package edu.purdue.cs;
 
-import android.os.Build;
-import android.support.v4.content.ContextCompat;
-import android.util.Log;
-import android.view.Window;
 import edu.purdue.cs.util.SystemUiHider;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-
-import com.parse.Parse;
-import com.parse.ParseAnalytics;
-import com.parse.ParseAnonymousUtils;
 import com.parse.ParseUser;
+import com.parse.ui.ParseLoginBuilder;
 
 
 /**
@@ -24,57 +15,47 @@ import com.parse.ParseUser;
  * @see SystemUiHider
  */
 public class SplashScreen extends Activity {
-    private static int SPLASH_TIME_OUT = 1500;
+    private static final int LOGIN_REQUEST = 0;
 
     private final Class startupClass = Startup.class;
+    private  ParseUser currentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-        final Window window = getWindow();
-        final int originalBarColor;
-        //this might be redundant
-        if(android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            originalBarColor = window.getNavigationBarColor();
-            window.setNavigationBarColor(ContextCompat.getColor(getApplicationContext(), R.color.app_theme));
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        currentUser = ParseUser.getCurrentUser();
+
+        if (currentUser != null) {
+            // The user is logged in. Direct to the startup activity.
+            Intent intent = new Intent(SplashScreen.this, Startup.class);
+            startActivity(intent);
+            finish();
         } else {
-            originalBarColor =  0;
+            // The user is not logged in. Direct to ParseLogin activity.
+            ParseLoginBuilder loginBuilder = new ParseLoginBuilder(
+                    SplashScreen.this);
+            startActivityForResult(loginBuilder.build(), LOGIN_REQUEST);
         }
-        Log.d("Splash", "Color" + originalBarColor);
+    }
 
-
-
-        new Handler().postDelayed(new Runnable() {
-
-            @Override
-            public void run() {
-                // This method will be executed once the timer is over.
-
-                // Change the bar color to fit the app theme.
-                if(android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) window.setNavigationBarColor(originalBarColor);
-
-                // The intent that the app is going to.
-                Intent intent;
-
-                ParseUser currentUser = ParseUser.getCurrentUser();
-
-                // Determine whether the current user is logged in
-                if (currentUser == null || ParseAnonymousUtils.isLinked(currentUser)) {
-                    // The user is a guest. Send the user to LoginActivity.class
-                    intent = new Intent(SplashScreen.this, LoginActivity.class);
-                    intent.putExtra("SUCCESS_ACTIVITY_CLASS", startupClass);
-                } else {
-                    // Send logged in users to Startup.class
-                    intent = new Intent(SplashScreen.this, startupClass);
-                }
-
-                // Redirect the user to the new activity.
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // The ParseLogin activity has returned.
+        if (requestCode == LOGIN_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                // The user successfully logged in.Direct to the startup activity.
+                Intent intent = new Intent(SplashScreen.this, Startup.class);
                 startActivity(intent);
-
-                // Close the current activity.
                 finish();
             }
-        }, SPLASH_TIME_OUT);
+        }
     }
 }
