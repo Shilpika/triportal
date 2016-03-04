5c71c8281c51e196bfb8e861bc66d7ba06133e84
Yibo Gou <i@bowiegou.com>
2016-03-04 15:02:20 -0500

    reverse to previous of adding a delay in splashscreen

--- Triportal/src/main/java/edu/purdue/cs/SplashScreen.java
+++ Triportal/src/main/java/edu/purdue/cs/SplashScreen.java
@@ -1,5 +1,6 @@
 package edu.purdue.cs;
 
+import android.os.Handler;
 import android.util.Log;
 import edu.purdue.cs.util.ui.SystemUiHider;
 import android.app.Activity;
@@ -34,19 +35,28 @@ public class SplashScreen extends Activity {
         super.onStart();
 
         currentUser = ParseUser.getCurrentUser();
+        new Handler().postDelayed(new Runnable() {
 
-        if (currentUser != null) {
-            // The user is logged in. Direct to the startup activity.
-            Log.d("USER ID",currentUser.getUsername());
-            Intent intent = new Intent(SplashScreen.this, startupClass);
-            startActivity(intent);
-            finish();
-        } else {
-            // The user is not logged in. Direct to ParseLogin activity.
-            ParseLoginBuilder loginBuilder = new ParseLoginBuilder(SplashScreen.this);
-            loginBuilder.useDefaultCredentials("bxd@cs.purdue.edu", "dunsmore");
-            startActivityForResult(loginBuilder.build(), LOGIN_REQUEST);
-        }
+            @Override
+            public void run() {
+                if (currentUser != null) {
+                    // The user is logged in. Direct to the startup activity.
+                    Log.d("USER ID",currentUser.getUsername());
+                    Intent intent = new Intent(SplashScreen.this, startupClass);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    // The user is not logged in. Direct to ParseLogin activity.
+                    ParseLoginBuilder loginBuilder = new ParseLoginBuilder(SplashScreen.this);
+                    loginBuilder.useDefaultCredentials("bxd@cs.purdue.edu", "dunsmore");
+                    startActivityForResult(loginBuilder.build(), LOGIN_REQUEST);
+                }
+            }
+        }, 1000);
+        /**
+        * TODO: the delay should be at leat 1000 ms but not always , until the data is fetched from server to load the first screen, this should be over
+        */
+
     }
 
     @Override
