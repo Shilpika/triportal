f7ba0790c1dc218fcbf97fa9c376c83778addab5
Rix <i@rix.li>
2016-03-04 00:17:38 -0500

    Add callback class type

--- Triportal/src/main/java/edu/purdue/cs/Itinerary.java
+++ Triportal/src/main/java/edu/purdue/cs/Itinerary.java
@@ -105,7 +105,7 @@ public class Itinerary extends ParseObject {
         return query.get(id);
     }
 
-    static public void getByIdInBackground(String id, GetCallback callback) {
+    static public void getByIdInBackground(String id, GetCallback<Itinerary> callback) {
         ParseQuery<Itinerary> query = ParseQuery.getQuery(Itinerary.class);
         query.fromLocalDatastore();
         query.getInBackground(id, callback);
