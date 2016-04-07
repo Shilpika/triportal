ae4949ada3da53346a330a7cc2de4f860c3f473d
Yucong Ma <1995charles@gmail.com>
2016-04-07 00:39:32 -0400

    User profile interface updated

--- Triportal/src/main/java/edu/purdue/cs/UserProfile.java
+++ Triportal/src/main/java/edu/purdue/cs/UserProfile.java
@@ -1,13 +1,20 @@
 package edu.purdue.cs;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.parse.Parse;
 import com.parse.ParseException;
@@ -15,6 +22,8 @@ import com.parse.ParseFile;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
+
+import org.w3c.dom.Text;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -34,10 +43,15 @@ public class UserProfile extends AppCompatActivity {
     private static Bitmap getAvatar() throws ParseException {
         ParseUser user = ParseUser.getCurrentUser();
         ParseFile file = user.getParseFile("avatar");
+        if(file == null){
+            return null;
+        }
         ByteArrayInputStream pngIn = new ByteArrayInputStream(file.getData());
         Bitmap avatar = BitmapFactory.decodeStream(pngIn);
         return avatar;
     }
+
+    public static int RESULT_LOAD_IMAGE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,16 +61,64 @@ public class UserProfile extends AppCompatActivity {
         Toolbar action = (Toolbar)findViewById(R.id.actionBar);
         setSupportActionBar(action);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
-
-
         ParseUser user = ParseUser.getCurrentUser();
-        //String username = user.getEmail();
-
         String username = user.getString("name");
-
         action.setTitle(username);
 
+        TextView User = (TextView)findViewById(R.id.userName);
+        User.setText(username);
 
+        TextView email = (TextView)findViewById(R.id.emailAdd);
+        email.setText(user.getEmail());
+
+        Button chooseImage = (Button)findViewById(R.id.chooseImage);
+        chooseImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent pick = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+                startActivityForResult(pick, RESULT_LOAD_IMAGE);
+            }
+        });
+
+        ImageView avatar = (ImageView)findViewById(R.id.avatarView);
+        try {
+            if(getAvatar() != null)
+                avatar.setImageBitmap(getAvatar());
+            else{
+                avatar.setImageDrawable(getResources().getDrawable(R.mipmap.ic_user_default));
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+
+            Cursor cursor = getContentResolver().query(selectedImage,
+                    filePathColumn, null, null, null);
+            cursor.moveToFirst();
+
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+
+            Bitmap pic = BitmapFactory.decodeFile(picturePath);
+            setAvatarInBackground(pic, new SaveCallback() {
+                @Override
+                public void done(ParseException e) {
+
+                }
+            });
+            ImageView ci = (ImageView)findViewById(R.id.avatarView);
+            ci.setImageBitmap(pic);
+        }
     }
 
     @Override
