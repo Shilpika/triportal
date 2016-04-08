8aacda5bed67d50a3c501b8b32f5b8de6247c291
Ge Yan <ivy951214@gmail.com>
2016-04-08 00:30:50 -0400

    add an add button in poi detail view

--- Triportal/src/main/java/edu/purdue/cs/PoiDetailView.java
+++ Triportal/src/main/java/edu/purdue/cs/PoiDetailView.java
@@ -3,6 +3,7 @@ package edu.purdue.cs;
 import android.app.Activity;
 import android.app.Fragment;
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -11,6 +12,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
@@ -31,6 +34,7 @@ public class PoiDetailView extends AppCompatActivity {
     private Itinerary itinerary;
     private Toolbar toolbar;
     private ListView poilist;
+    private String poi_id;
 
 
     @Override
@@ -38,17 +42,19 @@ public class PoiDetailView extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
 
-        setContentView(R.layout.daylistview);
+        setContentView(R.layout.poidetailview);
         ParseAnalytics.trackAppOpenedInBackground(getIntent());
         //View rootView =  inflater.inflate(R.layout.trip_tab, container, false);
-        toolbar = (Toolbar) findViewById(R.id.dayListViewActionBar);
-
+        toolbar = (Toolbar) findViewById(R.id.poi_detail_toolbar);
+        setSupportActionBar(toolbar);
 
 
         PoiDetailFragment fragment = PoiDetailFragment.newInstance();
-        fragment.setArguments(getIntent().getExtras());
+        Bundle extras = getIntent().getExtras();
+        fragment.setArguments(extras);
+        poi_id = extras.getString("poi_id");
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.day_list_frame, fragment, "fragment").commit();
+        transaction.replace(R.id.poi_detail_frame, fragment, "fragment").commit();
 
       /*  Log.d("DayListView It Id", itneraryID);
 
@@ -70,25 +76,30 @@ public class PoiDetailView extends AppCompatActivity {
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_poidetail, menu);
+        return true;
+    }
 
-//    private void getData(String id){
-//        try {
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//        toolbar.setTitle(itinerary.getTitle());
-//        View poiView = null;
-//        for(int i = 0; i < itinerary.getNumberOfDays(); i++){
-//            try {
-//                DayListViewAdapter dayListViewAdapter = new DayListViewAdapter(itinerary.getDays(), this);
-//                poiView = dayListViewAdapter.getView(i,poiView,(R.id.day_list));
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//            }
-//
-//
-//
-//        }
-//    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.add_action) {
+            Intent resultdata = new Intent();
+            resultdata.putExtra("poi_id", poi_id);
+            setResult(Activity.RESULT_OK, resultdata);
+            finish();
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
-
