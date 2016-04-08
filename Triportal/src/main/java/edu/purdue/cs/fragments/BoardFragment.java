50a18ea065e1cf1f5aa3c80a38e18a502834bd4d
Rix <i@rix.li>
2016-04-08 15:44:33 -0400

    Fix add one day bug (add the new day to mDayList)

--- Triportal/src/main/java/edu/purdue/cs/fragments/BoardFragment.java
+++ Triportal/src/main/java/edu/purdue/cs/fragments/BoardFragment.java
@@ -173,6 +173,7 @@ public class BoardFragment extends Fragment {
                             e1.printStackTrace();
                             return;
                         }
+                        BoardFragment.this.mDayList.add(day);
                         BoardFragment.this.addColumnList(poiList);
                     }
                 });
